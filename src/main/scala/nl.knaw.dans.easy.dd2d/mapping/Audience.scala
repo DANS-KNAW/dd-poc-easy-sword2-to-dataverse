becb7d6bcf855a8c398e65e2f40f1ce7713e5eb2
Chris <ChrisMuller80@users.noreply.github.com>
2020-11-01 13:54:23 +0100

    removed changes to classes

--- src/main/scala/nl.knaw.dans.easy.dd2d/mapping/Audience.scala
+++ src/main/scala/nl.knaw.dans.easy.dd2d/mapping/Audience.scala
@@ -15,9 +15,7 @@
  */
 package nl.knaw.dans.easy.dd2d.mapping
 
-import nl.knaw.dans.easy.dd2d.dataverse.json.{ FieldMap, JsonObject }
-
-import scala.xml.{ Node, XML }
+import scala.xml.Node
 
 /**
  * ddm:audience element with a NARCIS classification code in it.
@@ -39,35 +37,6 @@ object Audience extends BlockBasicInformation {
     "D7" -> "Business and Management",
     "E15" -> "Earth and Environmental Sciences"
   )
-
-  /**
-   * Creates a Map with Subject CV fields for a Compound Field
-   *
-   * @param node the audience element
-   * @return A JsonObject with Subject CV fields
-   */
-  def toBasicInformationBlockSubjectCv(node: Node): JsonObject = {
-    val termAndUrl = getTermAndUrl(node)
-    val m = FieldMap()
-    m.addPrimitiveField(SUBJECT_CV_VALUE, termAndUrl.term)
-    m.addPrimitiveField(SUBJECT_CV_VOCABULARY, SUBJECT_NARCIS_CLASSIFICATION)
-    m.addPrimitiveField(SUBJECT_CV_VOCABULARY_URI, termAndUrl.url)
-    m.toJsonObject
-  }
-
-  /**
-   * Gets term and url from the NARCIS classification
-   *
-   * @param node the audience element
-   * @return the Dataverse subject term and url
-   */
-  private def getTermAndUrl(node: Node): TermAndUrl = {
-    val narcisClassification = XML.loadFile("src/main/resources/narcis_classification.xml")
-    val element = narcisClassification.child.filter(_.attributes.exists(_.value.text contains node.text))
-    val term = element.headOption.flatMap(_.child.find(_.label == "prefLabel")).map(_.text).getOrElse("Other")
-    val url = element.headOption.flatMap(_.attributes.value.headOption).getOrElse(SUBJECT_NARCIS_CLASSIFICATION_URL).toString
-    TermAndUrl(term, url)
-  }
 
   /**
    * Returns the best match for this NARCIS classification code in the Dataverse subject vocabulary
